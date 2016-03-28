0c7aef6c2ea387abc49f16a27d821f151e4ec09d
MelMel <la_la_la_la_1@hotmail.com>
2016-03-27 22:11:52 -0600

    Can create a complete account
    Also, you can go back to the main page too with a click of a button

--- createAccount.php
+++ createAccount.php
@@ -8,15 +8,19 @@
 ?>
 <?php
 //Defining variables set to a default of null
-$username = $password = "";
+$username = $password = $realname = $country = $birth_date = $userEmail = "";
 
 if($_SERVER["REQUEST_METHOD"] == "POST")
 {
 	$username = getInput($_POST["user"]);
 	$password = getInput($_POST["pw"]);
+	$realname = getInput($_POST["realname"]);
+	$country = getInput($_POST["country"]);
+	$birth_date = getInput($_POST["birthdate"]);
+	$userEmail = getInput($_POST["userEmail"]);
 	
 	//Adding new information for an account
-	$newAccountQuery = "INSERT INTO Account (acc_name, password, real_name, country, birth_date, email) VALUES ('" .$username ."', '" . $password ."', 'Bob', '', '', 'some email')";
+	$newAccountQuery = "INSERT INTO Account (acc_name, password, real_name, country, birth_date, email) VALUES ('" .$username ."', '" . $password ."', '" . $realname . "', '" . $country . "', '" . $birth_date. "', '" . $userEmail . "')";
 	$result = mysqli_query($db, $newAccountQuery, MYSQLI_STORE_RESULT);
 	
 	
@@ -28,6 +32,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST")
 	}
 }
 
+
 function getInput($data)
 {
 	$data = trim($data);
@@ -36,6 +41,9 @@ function getInput($data)
 	return $data;
 }
 ?>
+<form action="index.php">
+	<input type="submit" value="Back to Main" style="position:relative;left:15px;top:281px;">
+</form>
 
 <form action="createAccount.php" method="post">
 	<fieldset>
@@ -44,8 +52,17 @@ function getInput($data)
 		<input type="text" name="user" required><br>
 		Password:<br>
 		<input type="password" name="pw" required><br>
-		<input type="submit" value="Submit">
+		Real name:<br>
+		<input type="text" name="realname" required><br>
+		Country:<br>
+		<input type="text" name="country"><br>
+		Date of birth:(day/month/year):<br>
+		<input type="text" name="birthdate"><br>
+		Email:<br>
+		<input type="text" name="userEmail" required><br>
+		<input type="submit" value="Submit" style="position:relative;left:120px;top:2px;">
 	</fieldset>
+	
 </form>
 
 </body>
