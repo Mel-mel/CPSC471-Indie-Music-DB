71327c729beb0051a8583cd2d8874a095b0c7d06
MelMel <la_la_la_la_1@hotmail.com>
2016-04-02 15:39:57 -0600

    Added some new pages

---
+++ editAccount.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	<link rel="stylesheet" href="styles.css">
+	</head>
+<body>
+<!--title will show up when you mouse over the text in that paragraph -->
+<p title ="Random stuff"> My first paragraph </p>
+<p><b>Just another random sentence that's bolded</b></p>
+
+
+<img src="images/senpai.gif" alt="shark-chan" style="width:480px;height:360px;">
+<img src="images/kill-you-so-hard.jpeg" alt="kill-you-so-hard" style="width:1440px;height:810px;">
+
+
+</body>
+</html>
